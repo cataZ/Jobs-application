7325a673b898f46cbab0e2b510c7bd6948891f9e
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-03-07 09:14:56 -0300

    modified getting token from schema instead of creating it

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,15 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 const { BadRequestError } = require('../errors') //no hace falta especificar el archivo ya que en la carpeta /errors, tenemos el index que lo deriva.
-const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
-	/*
-	const { name, email, password } = req.body
-
-	//con salt y la password a hashear se crea nuestra hashed password para enviarla a la bd de forma segura.
-	const salt = await bcrypt.genSalt(10)
-	const hashedPassword = await bcrypt.hash(password, salt)
-
-	//se crea un objeto tipo User de forma temporal para hacer el manejo de password.
-	//hay q remover la maxLength del schema User, ya que la password Hasheada envia muchos caracteres.
-	const tempUser = { name, email, password: hashedPassword }
-
-	//... operador de spread/rest lo usamos para acceder a todos los elementos de forma individual.)
-	*/ // Todo esto se realiza dentro del propio Schema que necesita el Hash.
-
 	const user = await User.create({ ...req.body })
-	res.status(StatusCodes.CREATED).json(user)
+	const token = user.createJWT()
+
+	res.status(StatusCodes.CREATED).json({
+		user: { name: user.name },
+		token,
+	})
 }
 
 const login = async (req, res) => {
