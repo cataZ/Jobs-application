5246fe6ffce5e9439a9d212fb89a5371d6a61017
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-03-08 21:52:41 -0300

    added create Job function

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,11 +1,19 @@
+const Job = require('../models/Job')
+const { StatusCodes } = require('http-status-codes')
+const { BadrequestError, NotFoundError } = require('../errors')
+
 const getAllJobs = async (req, res) => {
-	res.send('get all jobs')
+	const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+	res.status(StatusCodes.OK).json({ count: jobs.length, jobs })
 }
 const getSingleJob = async (req, res) => {
 	res.send('get singe job')
 }
 const createJob = async (req, res) => {
-	res.json(req.user)
+	//el user id es el que creamos con la payload de la JWT exitosa.
+	req.body.createdBy = req.user.userId
+	const job = await Job.create(req.body)
+	res.status(StatusCodes.CREATED).json({ job })
 }
 const updateJob = async (req, res) => {
 	res.send('update jobs')
