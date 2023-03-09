473c2941665ae84e367ff2d5dedcee122d6d23a2
Agustin Catanzaro <agustincatanzaro@gmail.com>
2023-03-09 20:07:52 -0300

    added delete, update and get getsingle Functions

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -6,20 +6,71 @@ const getAllJobs = async (req, res) => {
 	const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
 	res.status(StatusCodes.OK).json({ count: jobs.length, jobs })
 }
+
 const getSingleJob = async (req, res) => {
-	res.send('get singe job')
+	//job nos llega desde los parametros de la request
+	//user nos llega desde el middleware
+
+	// user:{userID}= nested destructuring, id sale de la route jobs/:id
+	const {
+		user: { userId },
+		params: { id: jobId },
+	} = req
+
+	const job = await Job.findOne({
+		_id: jobId,
+		createdBy: userId,
+	})
+	if (!job) {
+		throw new NotFoundError(`No job with id: ${jobId}`)
+	}
+	res.status(StatusCodes.OK).json({ job })
 }
+
 const createJob = async (req, res) => {
 	//el user id es el que creamos con la payload de la JWT exitosa.
 	req.body.createdBy = req.user.userId
 	const job = await Job.create(req.body)
 	res.status(StatusCodes.CREATED).json({ job })
 }
+
 const updateJob = async (req, res) => {
-	res.send('update jobs')
+	const {
+		body: { company, position },
+		user: { userId },
+		params: { id: jobId },
+	} = req
+
+	if (company === '' || position === '') {
+		throw new BadrequestError('Company and/or Position field cant be empty')
+	}
+	const job = await Job.findByIdAndUpdate(
+		{
+			_id: jobId,
+			createdBy: userId,
+		},
+		req.body,
+		{ new: true, runValidators: true }
+	)
+	if (!job) {
+		throw new NotFoundError(`No job with id: ${jobId}`)
+	}
+	res.status(StatusCodes.OK).json({ job })
 }
 const deleteJob = async (req, res) => {
-	res.send('delete jobs')
+	const {
+		body: { company, position },
+		user: { userId },
+		params: { id: jobId },
+	} = req
+	const job = await Job.findByIdAndRemove({
+		_id: jobId,
+		createdBy: userId,
+	})
+	if (!job) {
+		throw new NotFoundError(`No job with id: ${jobId}`)
+	}
+	res.status(StatusCodes.OK).send()
 }
 
 module.exports = {
